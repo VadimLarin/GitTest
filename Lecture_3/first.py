1543632801683fc4639fc0412c437d7ccd7f1a0e
VadimLarin <kholostovcontact@gmail.com>
2023-09-07 12:18:30 +0300

    тут еще не сделанная практика

--- Lecture_3/first.py
+++ Lecture_3/first.py
@@ -1,9 +1,9 @@
 new_name: str = input('type name: ')
 greet_message: str = 'hello biba'
 
-greet_message: str = (
-    greet_message[:-4] + new_name
-)
+#greet_message: str = (
+#    greet_message[:-4] + new_name
+#)
 
 
 print(greet_message)
---
+++ practice0709/simple_guess.py
@@ -0,0 +1,19 @@
+
+number: int = input('ввести число')
+x: int = number
+#def (
+#    )
+
+def guess (x):
+    while i == 0:
+        else:
+            for q in range(number):
+                if q * q == number:
+                    i = 1
+                    return q
+                    
+                                        
+
+print(x)
+                
+            
